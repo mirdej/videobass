283f8682203d4144f82be27fcb86cc5c3cbbec25
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2011-05-06 15:10:02 +0000

    changed manufacturer to anyma.ch/vamp because of a bug in current hi.tools that prevents correct product matching when multiple devices by same manufacturer are present

--- firmware/vamp/common/usbconfig.h
+++ firmware/vamp/common/usbconfig.h
@@ -235,8 +235,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x04
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'w', 'w', 'w', '.', 'a', 'n', 'y', 'm', 'a', '.', 'c', 'h'
-#define USB_CFG_VENDOR_NAME_LEN 12
+#define USB_CFG_VENDOR_NAME     'w', 'w', 'w', '.', 'a', 'n', 'y', 'm', 'a', '.', 'c', 'h','/','v','a','m','p'
+#define USB_CFG_VENDOR_NAME_LEN 17
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
