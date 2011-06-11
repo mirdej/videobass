30adb33ff8187d8889d6edf886ad24defd4d67ef
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2011-06-11 12:46:51 +0000

    Included missing jitter.jar. Release version 3.0.0. Still has some issues with file paths if there are spaces...

--- maxmsp/building/finish_build.sh
+++ maxmsp/building/finish_build.sh
@@ -1,22 +1,23 @@
 # COPY Shaders ----------------------------------------------------------------------------------------------------
-mkdir ~/Desktop/videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/patches/videobass/shaders/vb11.brcosa.jxs ~/Desktop/videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/patches/videobass/shaders/vb.5mix.jxs ~/Desktop/videobass.app/Contents/Resources/shaders/
+mkdir Videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/patches/videobass/shaders/vb11.brcosa.jxs Videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/patches/videobass/shaders/vb.5mix.jxs Videobass.app/Contents/Resources/shaders/
 # some shared shaders have to be included as well
-cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs ~/Desktop/videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl ~/Desktop/videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs Videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl Videobass.app/Contents/Resources/shaders/
 
 # COPY HID Usage tables for hi.in
-cp /Applications/Max5/patches/videobass/mxo/hi.tools/hi.usage* ~/Desktop/videobass.app/Contents/Resources/
+cp /Applications/Max5/patches/videobass/mxo/hi.tools/hi.usage* Videobass.app/Contents/Resources/
 
 # jit.openexr.mxo is needed by jit.net.send
-cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo ~/Desktop/videobass.app/Contents/Resources/
+cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo Videobass.app/Contents/Resources/
 
 # java classes and max.jar is needed by mxj
-cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar ~/Desktop/videobass.app/Contents/Resources/
-cp -R /Applications/Max5/Cycling\ \'74/java/classes ~/Desktop/videobass.app/Contents/
+cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar Videobass.app/Contents/Resources/
+cp /Applications/Max5/Cycling\ \'74/java/lib/jitter.jar Videobass.app/Contents/Resources/
+cp -R /Applications/Max5/Cycling\ \'74/java/classes Videobass.app/Contents/
 
 # ICON And Plist-----------------------------------------------------------------------------------------------
-cp /Applications/Max5/patches/videobass/building/vb.icon.icns ~/Desktop/videobass.app/Contents/Resources/
-cp /Applications/Max5/patches/videobass/building/Info.plist ~/Desktop/videobass.app/Contents/
+cp /Applications/Max5/patches/videobass/building/vb.icon.icns Videobass.app/Contents/Resources/
+cp /Applications/Max5/patches/videobass/building/Info.plist Videobass.app/Contents/
 
