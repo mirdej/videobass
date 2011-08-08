946eec523dffbf031d1ce31ed94f044c2b6463a4
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2011-08-08 12:45:16 +0000

    beta of version 3.1 with lumakey

--- branches/standard/maxmsp/building/finish_build.sh
+++ branches/standard/maxmsp/building/finish_build.sh
@@ -1,13 +1,15 @@
 # COPY Shaders ----------------------------------------------------------------------------------------------------
 mkdir Videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/patches/videobass/shaders/vb11.brcosa.jxs Videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/patches/videobass/shaders/vb.5mix.jxs Videobass.app/Contents/Resources/shaders/
+#cp ../shaders/vb11.brcosa.jxs Videobass.app/Contents/Resources/shaders/
+cp ../shaders/vb.mixer.jxs Videobass.app/Contents/Resources/shaders/
+cp ../shaders/vb.uyvy2rgba.jxs Videobass.app/Contents/Resources/shaders/
 # some shared shaders have to be included as well
 cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs Videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/color/cc.brcosa.jxs Videobass.app/Contents/Resources/shaders/
 cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl Videobass.app/Contents/Resources/shaders/
 
 # COPY HID Usage tables for hi.in
-cp /Applications/Max5/patches/videobass/mxo/hi.tools/hi.usage* Videobass.app/Contents/Resources/
+cp ../mxo/hi.tools/hi.usage* Videobass.app/Contents/Resources/
 
 # jit.openexr.mxo is needed by jit.net.send
 cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo Videobass.app/Contents/Resources/
@@ -18,6 +20,6 @@ cp /Applications/Max5/Cycling\ \'74/java/lib/jitter.jar Videobass.app/Contents/R
 cp -R /Applications/Max5/Cycling\ \'74/java/classes Videobass.app/Contents/
 
 # ICON And Plist-----------------------------------------------------------------------------------------------
-cp /Applications/Max5/patches/videobass/building/vb.icon.icns Videobass.app/Contents/Resources/
-cp /Applications/Max5/patches/videobass/building/Info.plist Videobass.app/Contents/
+cp vb.icon.icns Videobass.app/Contents/Resources/
+cp Info.plist Videobass.app/Contents/
 
