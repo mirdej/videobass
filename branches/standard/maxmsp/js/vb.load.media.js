b6e8db45f72dcb81c90e033b160fce75a9a754ed
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2011-09-19 14:37:17 +0000

    changed media loading to javascript based version. because of a bug in MaxMSP javascript that does not recognise filetypes when there is no explicit file extension, no file type check is done and any file will try to get loaded...

--- branches/standard/maxmsp/js/vb.load.media.js
+++ branches/standard/maxmsp/js/vb.load.media.js
@@ -1,13 +1,76 @@
-var files = new Array();
+var clip_path;
+var clip_count;
+var rec_path;
+var rec_count;
+var rec_slots;
+var media = new Array();
+var idx = 0;
 
-function clip_path(p) {
-	var f = new Folder (p);
-	post (p);
-	f.typelist = ['MooV'];
-	post (f.count);
+declareattribute("clip_path");
+declareattribute("rec_path");
+declareattribute("rec_slots");
+
+
+function conformpath(p) {
+	var path = p.join(" ");
+	 if (path.charAt(path.length-1) != "/") path += "/";
+	return path;
+}
+
+function zeropad(i) {
+	if (i < 10) return "00"+i;
+	if (i < 100) return "0"+i;
+	return i;
+}
+
+
+function start(){
+	media.length = 0;
+	clip_path = conformpath(clip_path);	
+	rec_path = conformpath(rec_path);	
+	
+	//-------------------- Add clips in "Videobass Clips"
+	var f = new Folder (clip_path);
+	clip_count = f.count;
 	while (!f.end) {
- 		post(f.filename);
- 	   	post();
-  	  	f.next();
+		 if (f.filename){			// js sees invisible files at beginning of folder???
+ 			media.push(clip_path+f.filename)
+  	 	}
+  	 	f.next();
   	}
+  	
+	//-------------------- Add recordings in "Videobass Recordings"
+  	f = new Folder (rec_path);
+	rec_count = f.count;
+	while (!f.end) {
+		 if (f.filename){			// js sees invisible files at beginning of folder???
+ 			media.push(rec_path+f.filename);
+ 			rec_slots--;
+  	 	}
+  	 	f.next();
+  	}
+	//-------------------- Add empty slots if applicable
+	idx = 0;
+	while (rec_slots > 0) {
+	 	rec_slots--;
+	 	idx++;
+	 	media.push("Recording"+zeropad(idx))
+	}
+	
+	
+	idx = 0;
+	bang();
+}
+
+
+
+function bang()	{
+	if (idx < media.length) {
+		outlet(0,"add",media[idx]);
+	} else {
+		outlet(0,"clip_count",clip_count);
+		outlet(0,"rec_count",rec_count);
+		outlet(0,"done");
+	}
+	idx++;
 }
