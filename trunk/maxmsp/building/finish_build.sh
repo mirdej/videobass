8cf538279827f500e4d734d0678545ae6775a443
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2012-01-07 15:53:33 +0000

    modified build script to automatically change version and add build number to Info.plist. change version.txt once, the rest is done by itself

--- trunk/maxmsp/building/finish_build.sh
+++ trunk/maxmsp/building/finish_build.sh
@@ -1,25 +1,38 @@
-# COPY Shaders ----------------------------------------------------------------------------------------------------
-mkdir Videobass.app/Contents/Resources/shaders/
-#cp ../shaders/vb11.brcosa.jxs Videobass.app/Contents/Resources/shaders/
-cp ../shaders/vb.mixer.jxs Videobass.app/Contents/Resources/shaders/
-cp ../shaders/vb.uyvy2rgba.jxs Videobass.app/Contents/Resources/shaders/
+# ----------------------------------------------------------------------------------------------------
+# configure paths
+APP_DIR=Videobass.app/Contents/
+MAX_DIR="/Applications/Max5/Cycling '74/"
+
+# ----------------------------------------------------------------------------------------------------
+#automatic paths
+RES_DIR=$APP_DIR"Resources/"
+SHADER_DIR=$RES_DIR"shaders/"
+
+echo --------------------- Finishing Build --------------------
+
+mkdir $SHADER_DIR
+#cp ../shaders/vb11.brcosa.jxs $SHADER_DIR
+cp ../shaders/vb.mixer.jxs $SHADER_DIR
+cp ../shaders/vb.uyvy2rgba.jxs $SHADER_DIR
 # some shared shaders have to be included as well
-cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs Videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/Cycling\ \'74/jitter-shaders/color/cc.brcosa.jxs Videobass.app/Contents/Resources/shaders/
-cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl Videobass.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs $SHADER_DIR
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/color/cc.brcosa.jxs $SHADER_DIR
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl $SHADER_DIR
 
 # COPY HID Usage tables for hi.in
-cp ../mxo/hi.tools/hi.usage* Videobass.app/Contents/Resources/
+cp ../mxo/hi.tools/hi.usage* $RES_DIR
 
 # jit.openexr.mxo is needed by jit.net.send
-cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo Videobass.app/Contents/Resources/
+cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo $RES_DIR
 
 # java classes and max.jar is needed by mxj
-cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar Videobass.app/Contents/Resources/
-cp /Applications/Max5/Cycling\ \'74/java/lib/jitter.jar Videobass.app/Contents/Resources/
-cp -R /Applications/Max5/Cycling\ \'74/java/classes Videobass.app/Contents/
+cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar $RES_DIR
+cp /Applications/Max5/Cycling\ \'74/java/lib/jitter.jar $RES_DIR
+cp -R /Applications/Max5/Cycling\ \'74/java/classes $APP_DIR
 
 # ICON And Plist-----------------------------------------------------------------------------------------------
-cp vb.icon.icns Videobass.app/Contents/Resources/
-cp Info.plist Videobass.app/Contents/
+cp vb.icon.icns $RES_DIR
+cp Info.plist $APP_DIR
 
+echo --------------------- Udpdating version --------------------
+perl update_version.pl
