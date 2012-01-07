40d467a29890371e1816f107a0d3d4c45ba30bd0
mirdejavel <mirdejavel@026b243e-0388-11df-9cd7-c9a19219af24>
2012-01-07 16:12:01 +0000

    update to readme and instructions

--- trunk/maxmsp/building/finish_build.sh
+++ trunk/maxmsp/building/finish_build.sh
@@ -24,6 +24,7 @@ cp ../mxo/hi.tools/hi.usage* $RES_DIR
 
 # jit.openexr.mxo is needed by jit.net.send
 cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo $RES_DIR
+#cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/Cg.framework $APP_DIR"Frameworks"
 
 # java classes and max.jar is needed by mxj
 cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar $RES_DIR
@@ -36,3 +37,6 @@ cp Info.plist $APP_DIR
 
 echo --------------------- Udpdating version --------------------
 perl update_version.pl
+
+
+
